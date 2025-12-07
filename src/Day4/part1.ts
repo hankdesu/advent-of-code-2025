ef758fd06ac710bad55b7f0fa3f4680eb9b508a0
HankChou <p30205@gmail.com>
2025-12-08 00:17:49 +0800

    feat(day4): Add day 4 answer

---
+++ src/Day4/part1.ts
@@ -0,0 +1,48 @@
+import { readInput } from "../utils";
+
+const input = await readInput(__dirname);
+const lines = input?.split("\n") ?? [];
+const matrix: string[][] = lines.map((line) => line.split(""));
+let answer = 0;
+
+const DIRECTIONS: [number, number][] = [
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+];
+
+matrix.forEach((line, i) => {
+  line.forEach((value, j) => {
+    if (value === "@") {
+      if (checkGrid(matrix, i, j)) {
+        answer++;
+      }
+    }
+  });
+});
+
+function checkGrid(matrix: string[][], i: number, j: number) {
+  let count = 0;
+
+  for (const [dx, dy] of DIRECTIONS) {
+    const ni = i + dx;
+    const nj = j + dy;
+
+    if (matrix[ni]?.[nj] === "@") {
+      count++;
+    }
+  }
+
+  if (count < 4) {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(answer);
